37bf0c02ab7ff9a7d887d07319b0264439ae9a60
rui hildt <rui@mullvad.net>
2026-04-30 00:27:33 +0200

    Fix endpoint route: change id to 'rankings' so endpoint is at /rankings

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 
 export default defineEndpoint({
-  id: 'mrdwc-rankings-2026',
+  id: 'rankings',
 
   handler: (router, { services, exceptions, env, database, getSchema }) => {
     const { ItemsService } = services;
     const { ServiceUnavailableException } = exceptions;
 
-    router.get('/rankings', async (_req, res) => {
+    router.get('/', async (_req, res) => {
       res.json({ message: 'MRDWC Rankings 2026 endpoint is running' });
     });
   },
